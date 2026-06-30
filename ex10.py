e7355a8731d782cb70ca5d33f0d8e0a7a879068d
patferman <patricia.ferman@gmail.com>
2026-06-30 19:56:39 -0300

    exercicios aula 9
    corrigindo exercicios na sala

---
+++ ex10.py
@@ -0,0 +1,18 @@
+#Escreva um programa que leia o salário de um funcionário e calcule o valor do imposto de renda a ser pago, considerando:
+
+#Faixa salarial até R$ 2.000,00: isento
+#R$ 2.000,01 até R$ 3.500,00: 10%
+#Acima de R$ 3.500,00: 20%.
+
+salario = float(input("digite o salário a calcular:    "))
+if salario < 2000:  
+    print("Isento")
+elif salario >= 3500:
+    print(salario * 0.2)
+else:    
+    print (salario * 0.1)
+
+
+
+
+
--- ex9.py
+++ ex9.py
@@ -1,1 +1,13 @@
 #Escreva um programa que simule uma calculadora simples. O programa deve receber dois números e uma operação (+, -, *, /). O resultado da operação deve ser exibido.
+numero1 = float(input("numero 1:  "))
+numero2 = float(input("numero 2:  "))
+operacao = input(" + " , " - ", "*" , "/")
+if operacao == +:
+resultado = numero1 + numero2
+elif operacao == -:
+resultado = numero1 - numero2
+elif operacao == *:
+resultado = numero 1 * numero2
+elif operacao == /:
+resultado = numero1 /numero2
+    print resultado

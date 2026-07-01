62cf245749705b003ec93fdcba8c16c36119f563
patferman <patricia.ferman@gmail.com>
2026-06-30 21:20:50 -0300

    organização
    organização das pastas

--- aula 9/ex10.py
+++ aula 9/ex10.py
@@ -4,14 +4,13 @@
 #R$ 2.000,01 até R$ 3.500,00: 10%
 #Acima de R$ 3.500,00: 20%.
 
-salario = float(input("digite o salário a calcular:    "))
+salario = float(input("digite o salário a calcular:R$   "))
 if salario < 2000:  
-    print("Isento")
+    print("Isento do pagamento de imposto de renda.")
 elif salario >= 3500:
-    print(salario * 0.2)
+    print(f' O imposto de renda é de R${salario * 0.2}')
 else:    
-    print (salario * 0.1)
-
+    print (f'O imposto de renda é de R${salario * 0.1}')
 
 
 

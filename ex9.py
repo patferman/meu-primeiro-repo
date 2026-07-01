056a5a84bdf7b5c0aa64d07d36dec7d38b647eaa
patferman <patricia.ferman@gmail.com>
2026-06-30 21:11:45 -0300

    Update ex9.py
    update

--- ex9.py
+++ ex9.py
@@ -1,13 +1,14 @@
 #Escreva um programa que simule uma calculadora simples. O programa deve receber dois números e uma operação (+, -, *, /). O resultado da operação deve ser exibido.
 numero1 = float(input("numero 1:  "))
 numero2 = float(input("numero 2:  "))
-operacao = input(" + " , " - ", "*" , "/")
+operacao = input(" Digitar a operação: ")
 if operacao == "+":
-    resultado = numero1 + numero2
+    print (numero1 + numero2)
 elif operacao == '-':
-    resultado = numero1 - numero2
+    print (numero1 - numero2)
 elif operacao == '*':
-    resultado = numero1 * numero2
+    print (numero1 * numero2)
 elif operacao == '/':
-    resultado = numero1 /numero2
-    print (resultado)
+    print (numero1 /numero2)
+else:
+    print("operação inválida")   
